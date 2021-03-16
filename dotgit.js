6d6fffaea3918814b16daf828354eacd13e454b9
davtur19 <dav.tur19@gmail.com>
2021-03-16 18:54:42 +0100

    More precise .env check

--- dotgit.js
+++ dotgit.js
@@ -40,7 +40,7 @@ const HG_MANIFEST_HEADERS = [
 ];
 
 const ENV_PATH = "/.env";
-const ENV_SEARCH = "^[A-Z_]*=";
+const ENV_SEARCH = "^[A-Z_]+=|^[#\\n\\r ][\\s\\S]*[A-Z_]+=";
 
 const GIT_TREE_HEADER = "tree ";
 const GIT_OBJECTS_PATH = "objects/";
@@ -209,7 +209,7 @@ function checkHg(url, visitedSite) {
 
 function checkEnv(url, visitedSite) {
     let to_check = url + ENV_PATH;
-    const search = new RegExp(ENV_SEARCH, "gm");
+    const search = new RegExp(ENV_SEARCH, "g");
 
     fetch(to_check, {
         redirect: "manual"
